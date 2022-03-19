7aca9df7a98682a44b2f2742b7a2598655b4cd90
Your Name <bernabas.abdeta@yahoo.com>
2022-03-19 06:35:26 -0400

    first step

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var firstQuestion = window.prompt("Would you like to start?");
+
+var TrueE1 = document.querySelector("#True");
+var FalseE1 = document.querySelector("#False");
+
+var quiz = function () {
+  var quiz1 = correct;
+  if (trueE1 === True) {
+ quizOne = True;
+  }
+  else {
+    false;
+  }
+}
